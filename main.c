4bb9c252306ed91383672ee7f4370dffa0c13bcb
Anshika <anshika@Anshikas-MacBook-Air.local>
2026-01-30 11:41:56 +0530

    my first commit

---
+++ main.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+
+// int sum (int a , int b){
+//     return a + b;
+// }
+// int main() {
+    // int a;
+    // a = 6;
+    // int anshi;
+    // anshi = 9;
+    // printf("%d" , a);
+    // printf("%d" , anshi);
+    // float b = 1.4;
+    // printf("%d" , b);
+    // char c;
+    // c = 'a';
+    // printf("%f" , c);
+//     printf("%d\n" , sum(10,20));
+//     return 0;
+// } 
+// int main() {
+    // int a = 19;
+    // float b = 7.989;
+    // char grade = 'a';
+    // printf("%d\n" , a);
+    // printf("%.2f\n" , b);
+    // printf("%c\n" , grade);
+//     int num = 3;
+//     printf("%d\n" , num);
+//     return 0;
+// }
+
+// int main() {
+//     int num = 90;
+//     printf("previous value : %d\n" , num);
+//     num = num+5;
+//     printf("add value : %d\n" , num);
+//     num = 10;
+//     printf("new value : %d\n" , num);
+//     return 0;
+// }
+
+
+// int main() {
+//     int num = 22;
+    
+//     // Finding size of num
+//     printf("%lu bytes\n", sizeof(num));
+//     return 0;
+// }
+
+// int main(){
+//     int a = 90;
+//     float b = 19.9;
+//     char c = 'A';
+//     double d = 9.9981;
+//     printf("int = %d\n" , a);
+//     printf("flot = %.2f\n" , b);
+//     printf("double data type = %.3lf\n" , d );
+//     printf("character = %c\n" , c);
+//     printf("the size of int: %lu\n" , sizeof(a));
+//     printf("size of float : %lu bytes\n" , sizeof(b));
+//     return 0;
+// }
+
+// void greet (){
+//     printf("hello world\n");
+//     printf("anshika\n");
+// }
+// int main(){
+//     greet();
+//     return 0;
+// }
+
+// int main () {
+//     int a = 25, b = 5;
+//     printf("a+b : %d\n" , a+b);
+//     printf("a-b : %d\n" , a-b);
+//     printf("a*b : %d\n" , a*b);
+//     printf("a/b : %d\n" , a/b);
+//     printf("a%%b : %d\n" , a%b);
+//     printf("+a : %d\n" , +a);
+//     printf("-a : %d\n" , -a);
+//     printf("a++ : %d\n" , a++);
+//     printf("a-- : %d\n" , a--);
+//     return 0;
+// }
+
