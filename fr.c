a0a6d99594c8a231f2370127a274280b8e33d4bc
anshikapawar3444-hue <anshikapawar3444@gmail.com>
2026-03-26 21:43:45 +0530

    Added fr.c

--- fr.c
+++ fr.c
@@ -142,3 +142,70 @@
 // float areaR (float a , float b) {
 //     return a*b;
 // }
+
+
+// RECURSION
+
+// #include <stdio.h>
+
+// void printhw (int count);
+// int main () {
+//     printhw(5);
+//     return 0;
+// }
+// void printhw (int count) {
+//     if (count == 0) 
+//     return ;
+//     printf("hello world\n");
+//     printhw(count-1);
+// }
+
+// QUES 4 sum of first n natural numbers
+
+// #include <stdio.h>
+// int sum(int n);
+
+// int main () {
+//     int n;
+//     scanf("%d",&n);
+//     printf("%d\n",sum(n));
+//     return 0;
+// }
+// int sum(int n) {
+//     if (n==1)
+//     return 1; // base case
+//     int sumNm1 = sum(n-1);
+//     int sumN = sumNm1 + n;
+//     return sumN;
+// }
+
+// QUES 5 factorial using recursion
+
+// #include <stdio.h>
+
+// int fac(int n);
+// int main () {
+//     printf("%d\n",fac(5));
+//     return 0;
+// }
+// int fac(int n) {
+//     if (n==0)
+//     return 1;
+//     int facNm1 = fac(n-1);
+//     int facN = facNm1 * n;
+//     return facN;
+// }
+
+#include <stdio.h>
+
+int fac(int n);
+int main () {
+    printf("%d\n",fac(5));
+    return 0;
+}
+int fac(int n) {
+    if (n==0)
+    return 1;
+    else 
+    return n*fac(n-1);
+}
