c12191184b989a57a934728d56183202d4a18458
anshikapawar3444-hue <anshikapawar3444@gmail.com>
2026-03-27 12:34:58 +0530

    Added class.c

--- class.c
+++ class.c
@@ -1,14 +1,31 @@
-#include <stdio.h>
-void swap (int a,int b) {
-    int temp;
-    temp = a;
-    a=b;
-    b=temp;
-    printf("a=%d,b=%d\n",a,b);
-}
-int main () {
-    int x,y;
-    scanf("%d %d",&x,&y);
-    swap(x,y);
-    return 0;
-}
+// #include <stdio.h>
+// void swap (int a,int b) {
+//     int temp;
+//     temp = a;
+//     a=b;
+//     b=temp;
+//     printf("a=%d,b=%d\n",a,b);
+// }
+// int main () {
+//     int x,y;
+//     scanf("%d %d",&x,&y);
+//     swap(x,y);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// void swap (int *a,int *b) {
+//     int temp ;
+//     temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
+// int main () {
+//     int x,y;
+//     scanf("%d %d",&x,&y);
+//     printf("before swap : x=%d,y=%d\n",x,y);
+//     swap(&x,&y);
+//     printf("after swap : x=%d, y=%d\n",x,y);
+//     return 0;
+// }
+
