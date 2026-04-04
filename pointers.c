d554bf580047aa7c339310f9445e106511d9c6b2
anshikapawar3444-hue <anshikapawar3444@gmail.com>
2026-04-04 22:08:29 +0530

    Updated pointers.c

--- pointers.c
+++ pointers.c
@@ -59,7 +59,6 @@
 //     return 0;
 // }
 
-// Ques 1
 // #include <stdio.h>
 
 // int main () {
@@ -96,4 +95,86 @@
 
 // Pointers in functions call.
 // it is of two types:
-// call by value and call by reference.
+// Call by value = We pass value of variable as argument . 
+// Call by reference = We pass address of variable as argument .
+
+// example of call by value
+
+// #include <stdio.h>
+// void square (int n);
+// int main () {
+//     int no = 4;
+//     square(no);
+//     printf("number = %d\n",no); // output = 4
+//     return 0;
+// }
+// void square (int n) {
+//     n = n*n;
+//     printf("square of number = %d\n",n);
+// }
+
+// example of call by reference 
+
+// #include <stdio.h>
+// void _square (int *n);
+// int main () {
+//     int no = 4;
+//     _square(&no);
+//     printf("number = %d\n",no); // output = 16
+//     return 0;
+// }
+// void _square (int *n) {
+//     *n = (*n)*(*n);
+//     printf("square of number = %d\n",*n);
+// }
+
+// QUES 1 swap two numbers 
+
+// #include <stdio.h>
+// void swap(int *a,int *b);
+// int main () {
+//     int a = 40,b=50;
+//     printf("Before swapping = %d %d\n",a,b);
+//     swap (&a,&b);
+//     printf("%d %d\n",a,b);
+//     return 0;
+// }
+// void swap (int *a,int *b) {
+//     int temp;
+//     temp = *a;
+//     *a = *b;
+//     *b = temp;
+//     printf("After swapping = %d %d\n",*a,*b);
+// }
+
+// QUES 2 Will the address output be same ?
+
+// #include <stdio.h>
+// void printAddress (int *n);
+// int main () {
+//     int n = 4;
+//     printAddress(&n);
+//     printf("address of n = %u\n",&n);
+//     return 0;
+// }
+// void printAddress (int *n) {
+//     printf("address of n = %u\n",&n);
+// }
+
+// QUES 3 sum,prod,avg return in main function. (these can be done by the help of pointers) 
+// #include <stdio.h>
+// void work(int a ,int b,int *sum,int *prod,int *avg);
+
+// int main () {
+//     int a = 3,b = 5;
+//     int sum , prod , avg;
+//     work(a,b,&sum,&prod,&avg);
+//     printf("sum = %d,prod = %d,avg = %d\n",sum,prod,avg);
+//     return 0;
+// }
+// void work(int a ,int b,int *sum,int *prod,int *avg) {
+//     *sum = a+b;
+//     *prod = a*b;
+//     *avg = (a+b)/2;
+// }
+
