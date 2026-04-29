c83fce8f745ec9c83939d7e206cc81800b16f808
anshikapawar3444-hue <anshikapawar3444@gmail.com>
2026-04-29 19:36:39 +0530

    Updated pointers2.c

--- pointers2.c
+++ pointers2.c
@@ -1,8 +1,83 @@
-#include <stdio.h>
+// ques based on pointers:
+// swap two no.
+// #include <stdio.h>
 
-int main () {
-    int a = 10;
-    int *p = &a;
-    
-    return 0;
-}
+// void swap(int *a,int *b) {
+//     int temp;
+//     temp = *a;
+//     *a=*b;
+//     *b=temp;
+//     printf("%d %d\n", *a,*b);
+// }
+// int main () {
+//     int x,y;
+//     scanf("%d %d",&x,&y);
+//     swap(&x,&y);
+//     return 0;
+// }
+
+// sum of two no.
+// #include <stdio.h>
+
+// int main () {
+//     int a,b;
+//     int *p=&a,*q=&b;
+//     scanf("%d %d",&a,&b);
+//     printf("%d\n",*p+*q);
+//     return 0;
+// }
+
+// largest of two no.
+// #include <stdio.h>
+
+// int main () {
+//     int a,b;
+//     int *p=&a,*q=&b;
+//     scanf("%d %d",&a,&b);
+//     if (*p>*q) {
+//         printf("%d is larger\n",*p);
+//     }
+//     else {
+//         printf("%d is larger\n",*q);
+//     }
+//     return 0;
+// }
+
+// rev array using pointers
+// #include <stdio.h>
+
+// int main () {
+//     int n;
+//     scanf("%d",&n);
+//     int a[n];
+//     for (int i=0;i<n;i++) {
+//         scanf("%d",&a[i]);
+//     }
+//     int *p=a;
+//     for (int i=n-1;i>=0;i--) {
+//         printf("%d ",*(p+i));
+//     }
+//     return 0;
+// }
+
+// count even no
+
+// #include <stdio.h>
+
+// int main () {
+//     int n,e=0;
+//     scanf("%d",&n);
+//     int a[n];
+//     for (int i=0;i<n;i++) {
+//         scanf("%d",&a[i]);
+//     }
+//     int *p=a;
+//     for (int i=0;i<n;i++) {
+//         if (*(p+i)%2==0) {
+//             e++;
+//         }
+//     }
+//     printf("%d",e);
+//     return 0;
+// }
+
