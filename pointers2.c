452fcdd721c0db96dd90c9023a71c6ffb1815c45
anshikapawar3444-hue <anshikapawar3444@gmail.com>
2026-04-30 22:24:11 +0530

    Updated pointers2.c

--- pointers2.c
+++ pointers2.c
@@ -1,3 +1,13 @@
+// Array of Pointers
+// Means: an array whose elements are pointers.
+
+// Pointer to Array
+// Means: one pointer pointing to the entire array.
+
+// Access Array Using Pointer
+// Means: Use a normal pointer to access elements of an array.
+
+
 // ques based on pointers:
 // swap two no.
 // #include <stdio.h>
@@ -131,3 +141,97 @@
 //         printf("%d ",arr[i]);
 // }
 
+// access array using pointer
+// #include <stdio.h>
+
+// int main () {
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for (int i=0;i<n;i++) {
+//         scanf("%d",&arr[i]);
+//     }
+//     int *p=arr;
+//     for (int i=0;i<n;i++) {
+//         printf("%d ",*(p+i));
+//     }
+//     return 0;
+// }
+
+// pointer to array 
+// #include <stdio.h>
+
+// int main () {
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+//     for (int i=0;i<n;i++) {
+//         scanf("%d",&arr[i]);
+//     }
+//     int (*p)[n]=&arr;
+//     for (int i=0;i<n;i++) {
+//         printf("%d",(*p)[i]);
+//     }
+//     return 0;
+// }
+
+// array of pointers
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a=10,b=20,c=30;
+//     int *arr[3];
+
+//     arr[0]=&a;
+//     arr[1]=&b;
+//     arr[2]=&c;
+
+//     for(int i=0;i<3;i++)
+//         printf("%d ", *arr[i]);
+
+//     return 0;
+// }
+
+// function and pointers
+
+// 1. passing pointer to a function (you pass an address of a variable as a argument ) = change original values , efficient for large data and arrays
+
+// #include <stdio.h>
+// void swap(int *a,int *b) {
+//     int temp;
+//     temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
+// int main () {
+//     int x,y;
+//     scanf("%d %d",&x,&y);
+//     swap(&x,&y);
+//     printf("%d %d\n",x,y);
+//     return 0;
+// }
+
+// 2. returning pointer from a function (function returns an address) 
+
+// #include <stdio.h>
+// int* larger(int *a,int *b) {
+//     if (*a>*b) {
+//         return a;
+//     }
+//     else {
+//         return b;
+//     }
+// }
+// int main () {
+//     int x,y;
+//     scanf("%d %d",&x,&y);
+//     int *p = larger (&x,&y);
+//     printf("%d ",*p);
+//     return 0;
+// }
+
+// void fun(int *p);        // passing pointer
+// int* fun();             // returning pointer
+// int (*fp)(int,int);     // function pointer
